dd6ab5b3e3a7f4368e231312bdc52d6cebe179e0
stepan <stepan-alekseev-87@mail.ru>
2021-10-14 02:27:39 +0700

    научился доставать элементы из JSON

--- attempt000/currencyRates/MainPage.xaml.cs
+++ attempt000/currencyRates/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Text.Json;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -31,7 +33,16 @@ namespace currencyRates
 
             var json = new WebClient().DownloadString("https://www.cbr-xml-daily.ru/daily_json.js");
 
-            window_for_tests.Text = json;
+            // JSON has the logic like { “key”: “value” } statement.
+            //Т.е. json - это:
+            //ключ -> значение(оно тоже м.б. ключом) -> значение(оно тоже м.б. ключом) и т.д.
+           var vr = json.Length;
+            //window_for_tests.Text = json;
+
+            JsonDocument doc = JsonDocument.Parse(json);
+            JsonElement root = doc.RootElement;
+            window_for_tests.Text = root.GetProperty("Valute").GetProperty("USD") .ToString();
+            //Console.WriteLine(root);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
